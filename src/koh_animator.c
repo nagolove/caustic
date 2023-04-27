b85255b351e002849ac559ba82f683459481bb94
nagolove <dekar.own@gmail.com>
2023-04-27 22:37:44 +0300

    loading lua files.

--- src/koh_animator.c
+++ src/koh_animator.c
@@ -3,6 +3,7 @@
 #include "koh_logger.h"
 #include <assert.h>
 #include <stdlib.h>
+#include "koh_lua_tools.h"
 #include "lua.h"
 #include "lauxlib.h"
 #include "stdio.h"
@@ -30,7 +31,28 @@ koh_Animator *koh_animator_new(koh_Animator_Def *def) {
 
     lua_State *lua = luaL_newstate();
 
+    char code[256] = {0};
+    snprintf(code, sizeof(code), "anim = loadfile('%s')", def->lua_file);
+
+    if (luaL_dostring(lua, code) != LUA_OK) {
+        trace(
+            "koh_animator_new: failed to luaL_dostring('%s')\n",
+            def->lua_file
+        );
+        exit(EXIT_FAILURE);
+    }
+
+    trace("koh_animator_new: [%s]\n", stack_dump(lua));
+    lua_getglobal(lua, "anim");
+    lua_pushstring(lua, "meta");
+    lua_gettable(lua, -2);
+    trace("koh_animator_new: [%s]\n", stack_dump(lua));
+
+    lua_pushstring(lua, "image");
+    lua_gettable(lua, -2);
+
     const char *image_fname = lua_tostring(lua, -1);
+    trace("koh_animator_new: [%s]\n", stack_dump(lua));
 
     char fname[512] = {0};
     snprintf(fname, sizeof(fname), "%s/%s", def->assets_dir, image_fname);
