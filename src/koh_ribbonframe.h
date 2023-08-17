f2cb34251c1f1fe82d69d9924385853354291f0c
nagolove <dekar.own@gmail.com>
2023-08-17 16:36:52 +0300

    Ribbon frame selection prototype

---
+++ src/koh_ribbonframe.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "raylib.h"
+
+struct RibbonFrame {
+    bool        exist;
+    Rectangle   rect;
+
+    int         mouse_button_bind;
+};
+
+struct RibbonFrameOpts {
+};
+
+void ribbonframe_init(struct RibbonFrame *rf, struct RibbonFrameOpts *opts);
+void ribbonframe_shutdown(struct RibbonFrame *rf);
+void ribbonframe_update(struct RibbonFrame *rf);
