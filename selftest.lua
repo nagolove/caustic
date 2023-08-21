ac79869a31857dd1745c0e6b3f42e31e3042d0e3
nagolove <dekar.own@gmail.com>
2023-08-21 15:11:24 +0300

    directories with test projects

---
+++ selftest.lua
@@ -0,0 +1,7 @@
+return {
+    "/home/nagolove/ecs-test",
+    "/home/nagolove/lua-tools-test",
+    "/home/nagolove/metaloader-test",
+    "/home/nagolove/set-test",
+    "/home/nagolove/strset-test",
+}
