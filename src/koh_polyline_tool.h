3fbb51f33d19ad7889594567f5b208e0ee74e073
nagolove <dekar.own@gmail.com>
2023-09-05 22:21:33 +0300

    Basic polyline works.

--- src/koh_polyline_tool.h
+++ src/koh_polyline_tool.h
@@ -10,14 +10,18 @@ struct PolylineTool {
 };
 
 struct PolylineToolOpts {
-    // Номер кнопки мыши из Raylib для работы с рамкой выделения
+    // Номер кнопки мыши из Raylib для работы с точками линиии
     // При -1 сохряняется текущее значение в PolylineTool
     int     mouse_button_bind;
     // Размер сетки для прилипания, >= 1
     //int     snap_size;
     //bool    snap;
-    //Color   line_color, handle_color;
-    //float   line_thick;
+    Color   line_color, handle_color, handle_selected_color;
+    float   line_thick;
+};
+
+struct PolylineToolDrawOpts {
+    //bool draw_axises;
 };
 
 void polyline_init(
@@ -29,13 +33,11 @@ void polyline_update_opts(
 void polyline_shutdown(struct PolylineTool *plt);
 void polyline_update(struct PolylineTool *plt, const Camera2D *cam);
 
-struct PolylineToolDrawOpts {
-    bool draw_axises;
-};
-
-// TODO: Рисование перекрестия и центра прямоугольника
 void polyline_draw(
     struct PolylineTool *plt, struct PolylineToolDrawOpts *opts
 );
 
-
+Vector2 *polyline_points_get(struct PolylineTool *plt, int *points_num);
+void polyline_points_set(
+    struct PolylineTool *plt, Vector2 *points, int points_num
+);
