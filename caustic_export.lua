6706c89dbee0136e7dabf83656d9bd4c36269ba2
nagolove <dekar.own@gmail.com>
2023-10-04 01:59:28 +0300

    Export sprite layers as separated png files.

--- caustic_export.lua
+++ caustic_export.lua
@@ -31,129 +31,197 @@ local function dprint(...)
     logfile:write("\n");
 end
 
-do
-    --[[
-    local spr = Sprite(32, 64)
-
-    app.useTool{
+-- Экспортировать слой в файл
+local function export(filename)
+    app.command.ExportSpriteSheet{
+        ui=false,
+        askOverwrite=false,
+        type=SpriteSheetType.HORIZONTAL,
+        columns=0,
+        rows=0,
+        width=0,
+        height=0,
+        bestFit=false,
+        textureFilename=filename,
+        dataFilename="",
+        dataFormat=SpriteSheetDataFormat.JSON_HASH,
+        borderPadding=0,
+        shapePadding=0,
+        innerPadding=0,
+        trim=false,
+        layerIndex = i,
+        ----mergeDuplicates=data.mergeDuplicates,
+        mergeDuplicates=false,
+        extrude=false,
+        openGenerated=false,
+        layer="",
+        tag="",
+        splitLayers=false,
+        --listLayers=layer,
+        listLayers=true,
+        listTags=true,
+        listSlices=true,
     }
-    
-    spr.properties.k = Uuid()
-    app.fs.makeDirectory("~/aseprite")
+end
 
-    spr:saveAs("~/aseprite/very_original_name_1.aseprite") -- works good
-    --spr:saveAs(app.fs.fileName("~/aseprite/very_original_name.aseprite"))
-    spr:close()
-    --]]
+local spr = app.activeSprite
 
-    --[[
-    local spr = app.activeSprite
-    if not spr then return app.alert "There is no active sprite" end
+if Sprite == nil then
+   -- Show error, no sprite active.
+   local dlg = MsgDialog("Error", "No sprite is currently active. Please, open a sprite first and run again.")
+   dlg:show()
+   return 1
+end
 
-    app.transaction(
-    function()
-        local bounds = Rectangle()
+--[[
+local spr = Sprite(32, 64)
 
-        logfile:write("bounds: " .. inspect(bounds) .. "\n")
+app.useTool{
+}
 
-        local layers = app.range.layers
-        logfile:write("layers: " .. inspect(layers) .. "\n")
+spr.properties.k = Uuid()
+app.fs.makeDirectory("~/aseprite")
 
-        local newLayer = spr:newLayer()
-        newLayer.name = "Glass Floor"
-        newLayer.opacity = 128
+spr:saveAs("~/aseprite/very_original_name_1.aseprite") -- works good
+--spr:saveAs(app.fs.fileName("~/aseprite/very_original_name.aseprite"))
+spr:close()
+--]]
 
-        for _,frame in ipairs(spr.frames) do
-            for _,layer in ipairs(layers) do
-                local img = Image(spr.spec)
-                for _,l in ipairs(layers) do
-                    local layerCel = l:cel(frame)
-                    if layerCel then
-                        img:drawImage(layerCel.image, layerCel.position)
-                        bounds = bounds:union(layerCel.bounds)
-                    end
+--[[
+local spr = app.activeSprite
+if not spr then return app.alert "There is no active sprite" end
+
+app.transaction(
+function()
+    local bounds = Rectangle()
+
+    logfile:write("bounds: " .. inspect(bounds) .. "\n")
+
+    local layers = app.range.layers
+    logfile:write("layers: " .. inspect(layers) .. "\n")
+
+    local newLayer = spr:newLayer()
+    newLayer.name = "Glass Floor"
+    newLayer.opacity = 128
+
+    for _,frame in ipairs(spr.frames) do
+        for _,layer in ipairs(layers) do
+            local img = Image(spr.spec)
+            for _,l in ipairs(layers) do
+                local layerCel = l:cel(frame)
+                if layerCel then
+                    img:drawImage(layerCel.image, layerCel.position)
+                    bounds = bounds:union(layerCel.bounds)
                 end
-
-                spr:newCel(newLayer, frame, img, Point(0, 0))
             end
-        end
 
-        local oldFrame = app.activeFrame
-        app.activeLayer = newLayer
-        for _,cel in ipairs(newLayer.cels) do
-            app.activeFrame = cel.frame
-            app.command.Flip{ target="mask", orientation="vertical" }
-            cel.position = Point(cel.position.x,
-            cel.position.y + 2*(bounds.y + bounds.height - spr.height/2))
-        end
-        app.activeFrame = oldFrame
-    end)
-    --]]
-
-    function export_layers()
-        -- Получить все слои
-        local active_sprite = app.activeSprite
-        dprint("active_sprite", active_sprite)
-        if not active_sprite then
-            return
-        end
-        layers = active_sprite.layers
-        --dprint("layers", layers)
-
-        -- Цикл по слоям
-        for i, layer in ipairs(layers) do
-            -- Получить имя файла
-            --filename = "layer_example_" .. layer.name .. ".png"
-            dprint("layer.name", layer.name)
-
-            -- Экспортировать слой в файл
-            --app.exportSprite(filename, layer)
-            app.command.ExportSpriteSheet {
-                --ui=true,
-                askOverwrite=true,
-                type=SpriteSheetType.HORIZONTAL,
-                columns=0,
-                rows=0,
-                width=0,
-                height=0,
-                bestFit=false,
-                --textureFilename="",
-                textureFilename="easysprite" .. layer.name,
-                dataFilename="",
-                dataFormat=SpriteSheetDataFormat.JSON_HASH,
-                filenameFormat="blaho-{title} ({layer}) {frame}.{extension}",
-                borderPadding=0,
-                shapePadding=0,
-                innerPadding=0,
-                trimSprite=false,
-                trim=false,
-                trimByGrid=false,
-                extrude=false,
-                --ignoreEmpty=false,
-                ignoreEmpty=true,
-                mergeDuplicates=false,
-                --openGenerated=false,
-                openGenerated=true,
-                --layer="",
-                layer=layer.name,
-                tag="",
-                splitLayers=false,
-                splitTags=false,
-                splitGrid=false,
-                listLayers=true,
-                listTags=true,
-                listSlices=true,
-                fromTilesets=false,
-            }
-
+            spr:newCel(newLayer, frame, img, Point(0, 0))
         end
     end
 
-    --dprint("app.command", app.command)
-    -- Вызвать функцию
-    export_layers()
+    local oldFrame = app.activeFrame
+    app.activeLayer = newLayer
+    for _,cel in ipairs(newLayer.cels) do
+        app.activeFrame = cel.frame
+        app.command.Flip{ target="mask", orientation="vertical" }
+        cel.position = Point(cel.position.x,
+        cel.position.y + 2*(bounds.y + bounds.height - spr.height/2))
+    end
+    app.activeFrame = oldFrame
+end)
+--]]
 
+function export_layers(output_path)
+    -- Получить все слои
+    local active_sprite = app.activeSprite
+    dprint("active_sprite", active_sprite)
+    if not active_sprite then
+        return
+    end
+    layers = active_sprite.layers
+    --dprint("layers", layers)
+   
+    local layers_visibility = {}
+
+    -- Цикл по слоям
+    -- Запоминание видимости
+    for i, layer in ipairs(layers) do
+        layers_visibility[i] = layer.isVisible
+    end
+
+    -- Все слои невидимы
+    for i, layer in ipairs(layers) do
+        layer.isVisible = false
+    end
+
+    -- Цикл по слоям
+    for i, layer in ipairs(layers) do
+        -- Получить имя файла
+        --filename = "layer_example_" .. layer.name .. ".png"
+        dprint("layer.name", layer.name)
+
+        local filename =    output_path .. 
+                            app.fs.fileTitle(app.activeSprite.filename) .. 
+                            "_" ..
+                            layer.name .. 
+                            ".png"
+        layer.isVisible = true
+        export(filename)
+        layer.isVisible = false
+    end
+
+    -- Восстановление видимости
+    for i, layer in ipairs(layers) do
+        layer.isVisible = layers_visibility[i]
+    end
 end
+
+-- Open main dialog.
+local dlg = Dialog("Export slices")
+dlg:file{
+    id = "directory",
+    label = "Output directory:",
+    --filename = app.activeSprite.filename,
+    open = false
+}
+--[[
+dlg:entry{
+    id = "filename",
+    label = "File name format:",
+    text = "{slicedata}" .. "." .. "{slicename}"
+}
+dlg:combobox{
+    id = 'format',
+    label = 'Export Format:',
+    option = 'png',
+    options = {'png', 'gif', 'jpg'}
+}
+dlg:slider{id = 'scale', label = 'Export Scale:', min = 1, max = 10, value = 1}
+dlg:check{id = "save", label = "Save sprite:", selected = false}
+dlg:button{id = "ok", text = "Export"}
+dlg:button{id = "cancel", text = "Cancel"}
+--]]
+dlg:button{id = "ok", text = "Export"}
+dlg:button{id = "cancel", text = "Cancel"}
+dlg:show()
+
+if not dlg.data.ok then 
+    return 0 
+end
+
+--dprint("app.command", app.command)
+-- Вызвать функцию
+
+local separator = string.sub(app.activeSprite.filename, 1, 1) == "/" and "/" or "\\"
+
+function dirname(str)
+   return str:match("(.*" .. separator .. ")")
+end
+
+--local filename = dlg.data.filename .. "." .. dlg.data.format
+local output_path = dirname(dlg.data.directory)
+dprint('output_path', output_path)
+export_layers(output_path)
 
 logfile:close()
 
