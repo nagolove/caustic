eef0f669646cf91007d326e61d9bf332905b9c92
nagolove <dekar.own@gmail.com>
2023-10-21 01:02:10 +0300

    Only t80 module.

--- src/koh_height_color.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <raylib.h>
-
-extern Color height_colors[];
-extern char *height_colors_names[];
-int get_height_colors_num(void);
-Color height_color(float value);
-//Color interp_color(Color a, Color b, float t);
