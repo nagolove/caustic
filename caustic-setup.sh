c4d82503c98b1a629ddb115e73b2ec1d27f8b897
nagolove <dekar.own@gmail.com>
2023-11-12 14:07:32 +0300

    using common variable

--- caustic-setup.sh
+++ caustic-setup.sh
@@ -16,21 +16,22 @@ sudo apt install libx11-dev libx11-dev libxinerama-dev libxcursor-dev libxi-dev 
 #luarocks install ansicolors --lua-version 5.4 --local
 #luarocks install luafilesystem --lua-version 5.4 --local
 
+LUA_VER="5.1"
 #sudo apt install libzip-dev 
-luarocks install luazip --local --lua-version 5.1
+luarocks install luazip --local --lua-version $LUA_VER
 
 #luarocks install lua-curl --local
 luarocks install lua-curl CURL_INCDIR=/usr/include/x86_64-linux-gnu --local
 
-luarocks install serpent --local --lua-version 5.1
-luarocks install tl --local --lua-version 5.1
-luarocks install luaposix --local --lua-version 5.1
-luarocks install inspect --local --lua-version 5.1
-luarocks install tabular --local --lua-version 5.1
-luarocks install ansicolors --local --lua-version 5.1
-luarocks install luafilesystem --local --lua-version 5.1
-luarocks install luasocket --local --lua-version 5.1
-luarocks install dkjson --local --lua-version 5.1
-luarocks install luaposix --local --lua-version 5.1
-luarocks install lanes --local --lua-version 5.1
-luarocks install compat53 --local --lua-version 5.1
+luarocks install serpent --local --lua-version $LUA_VER
+luarocks install tl --local --lua-version $LUA_VER
+luarocks install luaposix --local --lua-version $LUA_VER
+luarocks install inspect --local --lua-version $LUA_VER
+luarocks install tabular --local --lua-version $LUA_VER
+luarocks install ansicolors --local --lua-version $LUA_VER
+luarocks install luafilesystem --local --lua-version $LUA_VER
+luarocks install luasocket --local --lua-version $LUA_VER
+luarocks install dkjson --local --lua-version $LUA_VER
+luarocks install luaposix --local --lua-version $LUA_VER
+luarocks install lanes --local --lua-version $LUA_VER
+luarocks install compat53 --local --lua-version $LUA_VER
