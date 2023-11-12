b6657589339084ed95c736e21284bda19ce8fa93
nagolove <dekar.own@gmail.com>
2023-11-12 13:24:09 +0300

    Teal files directory changed

--- tlconfig.lua
+++ tlconfig.lua
@@ -5,11 +5,13 @@ return {
     --skip_compat53 = true,
     gen_target = "5.1",
     --global_env_def = "love",
-    --source_dir = "src",
+    source_dir = "tl_src",
     --build_dir = "app",
     include_dir = {
-        ".",
+        "tl_src",
     },
+    build_dir = ".",
+    --global_env_def = "tl_src",
     --include = include,
     --exclude = { },
 }
