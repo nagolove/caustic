22b01aed0c5f8677092ea6c22779409a8b51f768
nagolove <dekar.own@gmail.com>
2023-11-13 23:03:45 +0300

    comments removed

--- tlconfig.lua
+++ tlconfig.lua
@@ -2,15 +2,10 @@
 -- vim: fdm=marker
 
 return {
-    --skip_compat53 = true,
     gen_target = "5.1",
-    --global_env_def = "love",
     source_dir = "tl_src",
     include_dir = {
         "tl_src",
     },
     build_dir = "tl_dst",
-    --global_env_def = "tl_src",
-    --include = include,
-    --exclude = { },
 }
