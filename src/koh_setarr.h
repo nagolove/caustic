72b7fda8b4b0fa4c268fb74cd93d5fa542cdfe70
nagolove <dekar.own@gmail.com>
2023-11-27 23:06:37 +0300

    comments

--- src/koh_setarr.h
+++ src/koh_setarr.h
@@ -3,7 +3,8 @@
 #include "koh_hashers.h"
 
 struct SetIntArray {
-    int             *payload_arr, *hashes_arr;
+    int             *payload_arr, 
+                    *hashes_arr;
     int             cap, taken;
     HashFunction    hasher;
 };
@@ -11,6 +12,8 @@ struct SetIntArray {
 void setintarr_init(struct SetIntArray *h, int cap, HashFunction hasher);
 void setintarr_shutdown(struct SetIntArray *h);
 void setintarr_add(struct SetIntArray *h, int value);
+void setintarr_remove(struct SetIntArray *h, int value);
 bool setintarr_exists(struct SetIntArray *h, int value);
+// TODO: Добавить итератор для обхода всех элементов
 
 
