a3ac78616980314f0e4294e8a877a9ec5a2d76ce
nagolove <dekar.own@gmail.com>
2023-12-06 01:33:19 +0300

    New test

--- selftest.lua
+++ selftest.lua
@@ -1,9 +1,10 @@
 return {
+    "/home/nagolove/caustic-test-b2",
     "/home/nagolove/caustic-test-ecs",
     "/home/nagolove/caustic-test-lua",
     "/home/nagolove/caustic-test-metaloader",
+    "/home/nagolove/caustic-test-pallete",
+    "/home/nagolove/caustic-test-render",
     "/home/nagolove/caustic-test-set",
     "/home/nagolove/caustic-test-strset",
-    "/home/nagolove/caustic-test-b2",
-    "/home/nagolove/caustic-test-render",
 }
