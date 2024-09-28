c80b29f039b1d920914b5948041a15cdd9734ad9
nagolove <dekar.own@gmail.com>
2024-09-28 20:39:03 +0300

    Inline functions moved to ordinary

--- src/koh_hashers.c
+++ src/koh_hashers.c
@@ -1,3 +1,5 @@
+// vim: set colorcolumn=85
+// vim: fdm=marker
 #include "koh_hashers.h"
 
 #include <time.h>
@@ -14,3 +16,44 @@ struct HashFunctionDef koh_hashers[] = {
     { .f = koh_hasher_djb2, .fname = "djb2" },
     { .f = NULL, .fname = NULL },
 };
+
+const char *koh_hashers_name_by_funcptr(void *func_ptr) {
+    for (int i = 0; koh_hashers[i].f; i++) {
+        if (koh_hashers[i].f == func_ptr)
+            return koh_hashers[i].fname;
+    }
+    return NULL;
+}
+
+Hash_t koh_hasher_mum(const void *data, size_t len) {
+    // {{{
+    assert(koh_seed != 0);
+    return mum_hash(data, len, koh_seed);
+    // }}}
+}
+
+Hash_t koh_hasher_fnv64(const void *data, size_t len) {
+    // {{{
+	size_t i;
+	uint64_t h = 0xcbf29ce484222325ull;
+	const char *c = (char*)data;
+
+	for (i = 0; i < len; i++) {
+		h = (h * 0x100000001b3ll) ^ c[i];
+	}
+
+	return h;
+    // }}}
+}
+
+Hash_t koh_hasher_djb2(const void *data, size_t len) {
+    // {{{
+    Hash_t hash = 5381;
+    const char *str = data;
+
+    for (size_t i = 0; i < len; ++i)
+        hash = ((hash << 5) + hash) + str[i]; // hash * 33 + c 
+
+    return hash;
+    // }}}
+}
--- src/koh_hashers.h
+++ src/koh_hashers.h
@@ -1,3 +1,5 @@
+// vim: set colorcolumn=85
+// vim: fdm=marker
 #pragma once
 
 #include <stddef.h>
@@ -9,51 +11,17 @@
 typedef uint64_t Hash_t;
 typedef Hash_t (*HashFunction)(const void *data, size_t data_len);
 
-struct HashFunctionDef {
+typedef struct HashFunctionDef {
     HashFunction f;
     char *fname;
-};
+} HashFunctionDef;
 
 extern Hash_t koh_seed;
 extern struct HashFunctionDef koh_hashers[];
 
 void koh_hashers_init();
+const char *koh_hashers_name_by_funcptr(void *func_ptr);
 
-static inline Hash_t koh_hasher_mum(const void *data, size_t len) {
-    assert(koh_seed != 0);
-    return mum_hash(data, len, koh_seed);
-}
-
-static inline Hash_t koh_hasher_fnv64(const void *data, size_t len) {
-	size_t i;
-	uint64_t h = 0xcbf29ce484222325ull;
-	const char *c = (char*)data;
-
-	for (i = 0; i < len; i++) {
-		h = (h * 0x100000001b3ll) ^ c[i];
-	}
-
-	return h;
-}
-
-static inline Hash_t koh_hasher_djb2(const void *data, size_t len) {
-    Hash_t hash = 5381;
-    const char *str = data;
-
-    for (size_t i = 0; i < len; ++i)
-        hash = ((hash << 5) + hash) + str[i]; // hash * 33 + c 
-
-    return hash;
-}
-
-/*
-static inline Hash_t koh_hasher_djb2(const char *str) {
-    Hash_t hash = 5381;
-    int c;
-
-    while ((c = *str++))
-        hash = ((hash << 5) + hash) + c; // hash * 33 + c 
-
-    return hash;
-}
-*/
+Hash_t koh_hasher_mum(const void *data, size_t len);
+Hash_t koh_hasher_fnv64(const void *data, size_t len);
+Hash_t koh_hasher_djb2(const void *data, size_t len);
