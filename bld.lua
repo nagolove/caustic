b4acbe04eb4bfc9d5875bc475f32ce38d36c6f34
nagolove <dekar.own@gmail.com>
2024-10-21 18:46:59 +0300

    generated files.

--- bld.lua
+++ bld.lua
@@ -8,7 +8,19 @@ return {
         exclude = {
             "stage_template.h",
             "stage_template.c",
-        }
+        },
+        codegen = { {
+            external = function()
+                --os.execute("cd src")
+                os.execute(
+                    "cd src && bin2c -t -d koh_lua_tabular.h " ..
+                    "-o koh_lua_tabular.c ../tl_dst/tabular.lua"
+                )
+                --print("HELLO")
+                --os.exit()
+                --print(lfs.currentdir())
+            end
+        }, }
     },
 }
 
