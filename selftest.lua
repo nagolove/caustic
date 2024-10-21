63967ea9c35b8bcd1b56660532a25a6f5547d78f
nagolove <dekar.own@gmail.com>
2024-10-21 18:46:50 +0300

    Set tests were disabled. Now HTable used for sets.

--- selftest.lua
+++ selftest.lua
@@ -10,6 +10,6 @@ return {
     "/home/nagolove/caustic-test-metaloader",
     "/home/nagolove/caustic-test-pallete",
     "/home/nagolove/caustic-test-render",
-    "/home/nagolove/caustic-test-set",
-    "/home/nagolove/caustic-test-strset",
+    --"/home/nagolove/caustic-test-set",
+    --"/home/nagolove/caustic-test-strset",
 }
