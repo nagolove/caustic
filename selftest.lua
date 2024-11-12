6d29b670f8d8f393dccbcf48e2b185a2ef4fff9c
nagolove <dekar.own@gmail.com>
2024-11-12 14:11:44 +0300

    Удалены неиспользуемые тесты.

--- selftest.lua
+++ selftest.lua
@@ -3,13 +3,10 @@ return {
     "/home/nagolove/caustic-test-qtree",
     "/home/nagolove/caustic-test-b2",
     "/home/nagolove/caustic-test-common",
-    "/home/nagolove/caustic-test-ecs",
     "/home/nagolove/caustic-test-ecs-2",
     "/home/nagolove/caustic-test-htable",
     "/home/nagolove/caustic-test-lua-tools",
     "/home/nagolove/caustic-test-metaloader",
     "/home/nagolove/caustic-test-pallete",
     "/home/nagolove/caustic-test-render",
-    --"/home/nagolove/caustic-test-set",
-    --"/home/nagolove/caustic-test-strset",
 }
