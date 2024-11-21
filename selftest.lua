2bfa9a4394e9561ce1e046328fbab4fbe669bc80
nagolove <dekar.own@gmail.com>
2024-11-22 00:45:49 +0300

    Проект с вызовом внутреннего теста новой ecs

--- selftest.lua
+++ selftest.lua
@@ -1,12 +1,13 @@
 return {
-    "/home/nagolove/caustic-test-mm_arena",
-    "/home/nagolove/caustic-test-qtree",
     "/home/nagolove/caustic-test-b2",
     "/home/nagolove/caustic-test-common",
     "/home/nagolove/caustic-test-ecs-2",
+    "/home/nagolove/caustic-test-ecs-3",
     "/home/nagolove/caustic-test-htable",
     "/home/nagolove/caustic-test-lua-tools",
     "/home/nagolove/caustic-test-metaloader",
+    "/home/nagolove/caustic-test-mm_arena",
     "/home/nagolove/caustic-test-pallete",
+    "/home/nagolove/caustic-test-qtree",
     "/home/nagolove/caustic-test-render",
 }
