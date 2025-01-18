4a2d627043336dc8c0a09751cd07a4e5f84c1fd0
nagolove <dekar.own@gmail.com>
2025-01-18 03:23:55 +0300

    Новый тест

--- selftest.lua
+++ selftest.lua
@@ -1,6 +1,7 @@
 return {
     "/home/nagolove/caustic-test-b2",
     "/home/nagolove/caustic-test-common",
+    "/home/nagolove/caustic-test-strbuf",
     --"/home/nagolove/caustic-test-ecs-2",
     "/home/nagolove/caustic-test-ecs-3",
     "/home/nagolove/caustic-test-htable",
