b3379cb054376f647944f65306c1ace6e711e38c
nagolove <dekar.own@gmail.com>
2025-02-13 18:07:27 +0300

    Создание каталога с проектом по шаблону.

--- tl_dst/caustic.lua
+++ tl_dst/caustic.lua
@@ -813,7 +813,7 @@ local function build_resvg(_)
    print('build_resvg')
    ut.push_current_dir()
    cmd_do("cargo build --release")
-   lfs.chdir("crates/c-api")
+   chdir("crates/c-api")
    cmd_do("cargo build --release")
    ut.pop_dir()
 end
@@ -834,7 +834,7 @@ end
 local function build_remotery(_)
    print('build_remotery', lfs.currentdir())
    ut.push_current_dir()
-   lfs.chdir("lib")
+   chdir("lib")
    cmd_do("gcc -c Remotery.c  -I lib -pthread -lm")
    cmd_do("ar -rcs  \"libremotery.a\" Remotery.o")
    ut.pop_dir()
@@ -1868,20 +1868,15 @@ end
 
 
 
+
 local parser_setup = {
 
 
    project = {
       options = { "-n --name" },
-      summary = [[Work with projects
-    new - create new project from template
-    list - list of all registed projects in CAUSTIC_PATH/projects.lua
-]],
-      arguments = {
-         { "new", 1 },
-         { "list", 1 },
-      },
+      summary = [[create project with name in current directory]],
    },
+
    stage = {
       options = { "-n --name" },
       summary = [[Work with stages.
@@ -2240,7 +2235,379 @@ local function stage_new(_)
 end
 
 function actions.project(_args)
-   print("actions.project");
+   print("actions.project:", inspect(_args))
+
+   local project_name = _args.name
+   if not project_name then
+      print("There is no project name")
+      return
+   end
+
+   local ok = mkdir(project_name)
+   if not ok then
+      print(format(
+      "Could not create '%s', may be directory exists",
+      project_name))
+
+      return
+   end
+
+   ut.push_current_dir()
+   chdir(project_name)
+
+   mkdir("src")
+   mkdir("assets")
+
+   local tlconfig_lua =
+
+   [[
+-- vim: set colorcolumn=85
+-- vim: fdm=marker
+
+return {
+    --skip_compat53 = true,
+    --gen_target = "5.1",
+    --global_env_def = "love",
+    --source_dir = "src",
+    --build_dir = "app",
+    include_dir = {
+        "assets",
+    },
+    gen_compat = "off",
+    --include = include,
+    --exclude = { },
+}
+]]
+
+
+   local bld_tl =
+
+   [[
+-- vim: set colorcolumn=85
+-- vim: fdm=marker
+
+return {
+
+    -- еденица трансляции
+    {
+        -- список отклченных для данной сборки зависимостей
+        -- {{{
+        not_dependencies = {
+
+            -- LuaFileSystem, имеет проблемы со сборкой на Lua 5.4, требует
+            -- патча исходного кода
+            "lfs",
+
+            -- не линкуется статически, приложение требует libresvg.so, но не
+            -- использует
+            "resvg",
+
+        },
+        -- }}}
+
+        -- результат компиляции и линковки
+        artifact = "lapsha",
+        -- файл с функцикй main()
+        main = "main.c",
+        -- каталог с исходными текстами
+        src = "src",
+        -- исключать следующие имена, можно использовать Lua шаблоны
+        exclude = {
+            --"t80_stage_empty.c",
+        },
+        -- список дефайнов для отладочной сборки
+        debug_define = {
+            ["BOX2C_SENSOR_SLEEP"] = "1",
+        },
+        -- список дефайнов для релизной сборки
+        release_define = {
+            --["T80_NO_ERROR_HANDLING"] = "1",
+            --["KOH_NO_ERROR_HANDLING"] = "1",
+        },
+    },
+    -- конец еденицы трансляции
+
+}
+]]
+
+
+   local f = io.open("tlconfig.lua", "w")
+   f:write(tlconfig_lua)
+   f:close()
+
+   f = io.open("bld.tl", "w")
+   f:write(bld_tl)
+   f:close()
+
+   cmd_do("cyan build")
+
+   local main_c =
+
+   [[// vim: set colorcolumn=85
+// vim: fdm=marker
+
+#include "koh_script.h"
+#include "koh_stages.h"
+#if defined(PLATFORM_WEB)
+#include <emscripten/emscripten.h>
+#else
+#include <signal.h>
+#include <unistd.h>
+#include <execinfo.h>
+#include <dirent.h>
+#endif
+
+#define CIMGUI_DEFINE_ENUMS_AND_STRUCTS
+
+// include {{{
+
+#include "cimgui.h"
+#include "cimgui_impl.h"
+#include "koh.h"
+#include "raylib.h"
+#include <assert.h>
+#include "koh_common.h"
+#include <memory.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// }}}
+
+Color color_background_clear = GRAY;
+
+struct Input2 *input2 = NULL;
+//struct t80_args app_args;
+
+//#define LAPTOP  1
+
+static double last_time = 0.;
+
+#ifdef LAPTOP
+static const int screen_width_desk = 1920 * 1;
+static const int screen_height_desk = 1080 * 1;
+#else
+static const int screen_width_desk = 1920 * 2;
+static const int screen_height_desk = 1080 * 2;
+#endif
+
+#ifdef PLATFORM_WEB
+static const int screen_width_web = 1920;
+static const int screen_height_web = 1080;
+#endif
+static HotkeyStorage hk_store = {};
+static StagesStore *ss = NULL;
+static Camera2D cam = {
+    .zoom = 1.,
+};
+
+static void console_on_enable(HotkeyStorage *hk_store, void *udata) {
+    trace("console_on_enable:\n");
+    //hotkey_group_enable(hk_store, HOTKEY_GROUP_FIGHT, false);
+}
+
+static void console_on_disable(HotkeyStorage *hk_store, void *udata) {
+    trace("console_on_disable:\n");
+    //hotkey_group_enable(hk_store, HOTKEY_GROUP_FIGHT, true);
+}
+
+static void gui_render() {
+    rlImGuiBegin();
+
+    stages_gui_window(ss);
+    stage_active_gui_render(ss);
+
+    bool open = false;
+    igShowDemoWindow(&open);
+
+    rlImGuiEnd();
+}
+
+static void update(void) {
+    koh_camera_process_mouse_drag(&(struct CameraProcessDrag) {
+        .mouse_btn = MOUSE_BUTTON_RIGHT,
+        .cam = &cam,
+    });
+    koh_camera_process_mouse_scale_wheel(&(struct CameraProcessScale) {
+        .dscale_value = 0.1,
+        .cam = &cam,
+        .modifier_key_down = KEY_LEFT_SHIFT,
+    });
+
+    koh_fpsmeter_frame_begin();
+
+    inotifier_update();
+
+    BeginDrawing();
+    ClearBackground(color_background_clear);
+
+    hotkey_process(&hk_store);
+    console_check_editor_mode();
+
+    // XXX: Почему не отображается?
+    console_write("fps %d\n", GetFPS());
+
+    koh_fpsmeter_draw();
+
+    Vector2 mp = Vector2Add(
+        GetMousePosition(), GetMonitorPosition(GetCurrentMonitor())
+    );
+    console_write("%s", Vector2_tostr(mp));
+
+    console_update();
+    gui_render();
+
+    EndDrawing();
+
+    koh_fpsmeter_frame_end();
+}
+
+#if !defined(PLATFORM_WEB)
+
+void sig_handler(int sig) {
+    printf("sig_handler: %d signal catched\n", sig);
+    /*
+    // XXX:
+    if (__STDC_VERSION__ >=201710L) {
+    } else
+        printf("sig_handler: %s signal catched\n", strsignal(sig));
+    */
+    koh_backtrace_print();
+    KOH_EXIT(EXIT_FAILURE);
+}
+#endif
+
+int main(int argc, char **argv) {
+#if !defined(PLATFORM_WEB)
+    signal(SIGSEGV, sig_handler);
+#endif
+
+    SetTraceLogCallback(koh_log_custom);
+
+    koh_hashers_init();
+    logger_init();
+
+    const char *wnd_name = "lapsha";
+
+    SetTraceLogLevel(LOG_WARNING);
+
+#ifdef PLATFORM_WEB
+    SetConfigFlags(FLAG_MSAA_4X_HINT);  // Set MSAA 4X hint before windows creation
+    InitWindow(screenWidth_web, screenHeight_web, wnd_name);
+    SetTraceLogLevel(LOG_ALL);
+#else
+    //SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_FULLSCREEN_MODE);  // Set MSAA 4X hint before windows creation
+    SetConfigFlags(FLAG_MSAA_4X_HINT | FLAG_WINDOW_UNDECORATED);  // Set MSAA 4X hint before windows creation
+    InitWindow(screen_width_desk, screen_height_desk, wnd_name);
+    SetTraceLogLevel(LOG_ALL);
+    // FIXME: Работает только на моей конфигурации, сделать опцией
+    // К примеру отрабатывать только на флаг -DDEV
+#ifndef LAPTOP
+    SetWindowPosition(GetMonitorPosition(1).x, 0);
+#endif
+    //dotool_setup_display(testing_ctx);
+#endif
+
+    SetExitKey(KEY_NULL);
+
+    sc_init();
+    inotifier_init();
+    logger_register_functions();
+
+    koh_fpsmeter_init();
+    sc_init_script();
+    koh_common_init();
+
+    ss = stage_new(&(struct StageStoreSetup) {
+        .stage_store_name = "main",
+        .l = sc_get_state(), // TODO: Зачем передавать Lua состояние?
+    });
+
+    hotkey_init(&hk_store);
+
+    // XXX: Требуется включение и выключение
+    InitAudioDevice();
+    CloseAudioDevice();
+
+    sfx_init();
+    koh_music_init();
+    koh_render_init();
+
+    struct igSetupOptions opts = {
+        .dark = false,
+        .font_size_pixels = 35,
+        .font_path = "assets/DejaVuSansMono.ttf",
+        .ranges = (ImWchar[]){
+            0x0020, 0x00FF, // Basic Latin + Latin Supplement
+            0x0400, 0x044F, // Cyrillic
+            // XXX: symbols not displayed
+            // media buttons like record/play etc. Used in dotool_gui()
+            0x23CF, 0x23F5,
+            0,
+        },
+    };
+    rlImGuiSetup(&opts);
+
+    // stage_init(ss);
+
+    console_init(&hk_store, &(struct ConsoleSetup) {
+        .on_enable = console_on_enable,
+        .on_disable = console_on_disable,
+        .udata = NULL,
+        .color_text = BLACK,
+        .color_cursor = BLUE,
+        .fnt_size = 32,
+        .fnt_path = "assets/fonts/DejavuSansMono.ttf",
+    });
+
+    last_time = GetTime();
+
+#if defined(PLATFORM_WEB)
+    emscripten_set_main_loop(update, 60, 1);
+#else
+    //dotool_send_signal(testing_ctx);
+
+    SetTargetFPS(120 * 3);
+    while (!WindowShouldClose() && !koh_cmn()->quit) {
+        update();
+    }
+
+#endif
+
+    stage_shutdown(ss);// добавить в систему инициализации
+    koh_music_shutdown();       // добавить в систему инициализации
+    koh_fpsmeter_shutdown(); // добавить в систему инициализации
+    koh_render_shutdown();// добавить в систему инициализации
+    console_shutdown();// добавить в систему инициализации
+    hotkey_shutdown(&hk_store);// добавить в систему инициализации, void*
+    if (ss) {
+        stage_free(ss);
+        ss = NULL;
+    }
+    koh_common_shutdown();// добавить в систему инициализации
+    sc_shutdown();// добавить в систему инициализации
+    sfx_shutdown();// добавить в систему инициализации
+    inotifier_shutdown();// добавить в систему инициализации
+    rlImGuiShutdown();// добавить в систему инициализации
+    CloseWindow();// добавить в систему инициализации
+
+    //dotool_free(testing_ctx);
+    logger_shutdown();
+
+    return EXIT_SUCCESS;
+}
+]]
+
+
+   f = io.open("src/main.c", "w")
+   f:write(main_c)
+   f:close()
+
+   cmd_do(format("cp -r %s/assets .", path_caustic))
+   cmd_do("koh compile_flags")
+   cmd_do("koh make -c")
+
+   ut.pop_dir()
 end
 
 function actions.stage(_args)
@@ -4011,7 +4378,7 @@ end
 
 local function backup(dep)
    ut.push_current_dir()
-   lfs.chdir(path_rel_third_party)
+   chdir(path_rel_third_party)
 
    print('backup')
    print('currentdir', lfs.currentdir())
@@ -4045,10 +4412,10 @@ end
 
 local function _update(dep)
    ut.push_current_dir()
-   lfs.chdir(path_caustic)
+   chdir(path_caustic)
    if dep.update then
       backup(dep)
-      lfs.chdir(path_rel_third_party .. "/" .. dep.dir)
+      chdir(path_rel_third_party .. "/" .. dep.dir)
       dep.update(dep)
    end
    ut.pop_dir()
