5e0f078db116428cde57c7e6cd21cc3de899567d
nagolove <dekar.own@gmail.com>
2025-02-21 02:32:33 +0300

    Скриптер.

--- projects.lua
+++ projects.lua
@@ -1,11 +1,18 @@
 --[[
 Список зарегистрированных в системе проектов. Необходим для работы подсистемы
 сцен в командной утилите щелочи.
-TODO: Сделать возможность пересборки всех проектов.
 --]]
 return {
+    "/home/nagolove/koh-scripter",
+    "/home/nagolove/koh-fnt-vector",
+    "/home/nagolove/koh-imgui_automation",
+    "/home/nagolove/koh-stages",
+    "/home/nagolove/koh-lapsha",
     "/home/nagolove/koh-async-loader",
-    "/home/nagolove/koh-t80",
-    "/home/nagolove/koh-svg",
+    "/home/nagolove/koh-pomidor",
+    "/home/nagolove/koh-colorworms-tuned",
+
+    "/home/nagolove/koh-mgc",
     "/home/nagolove/koh-2048",
+    "/home/nagolove/koh-t80",
 }
