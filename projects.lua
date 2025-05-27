ff39ca3b3406ea676a535b2a4bec3891bb972d11
nagolove <dekar.own@gmail.com>
2025-05-28 02:08:45 +0300

    Обновлен

--- projects.lua
+++ projects.lua
@@ -3,16 +3,36 @@
 сцен в командной утилите щелочи.
 --]]
 return {
-    "/home/nagolove/koh-scripter",
+
+    --[[
+    "/home/nagolove/koh-2048",
+    "/home/nagolove/koh-async-loader",
+    "/home/nagolove/koh-colorworms-tuned",
     "/home/nagolove/koh-fnt-vector",
     "/home/nagolove/koh-imgui_automation",
-    "/home/nagolove/koh-stages",
     "/home/nagolove/koh-lapsha",
-    "/home/nagolove/koh-async-loader",
-    "/home/nagolove/koh-pomidor",
-    "/home/nagolove/koh-colorworms-tuned",
-
     "/home/nagolove/koh-mgc",
-    "/home/nagolove/koh-2048",
+    "/home/nagolove/koh-pomidor",
+    "/home/nagolove/koh-scripter",
+    "/home/nagolove/koh-stages",
     "/home/nagolove/koh-t80",
+    --]]
+    --
+
+    "/home/nagolove/koh-2048",
+    "/home/nagolove/koh-async-loader",
+    "/home/nagolove/koh-colorworms-tuned",
+    --"/home/nagolove/koh-ctl",
+    "/home/nagolove/koh-fnt-vector",
+    "/home/nagolove/koh-hexia",
+    "/home/nagolove/koh-imgui_automation",
+    "/home/nagolove/koh-lapsha",
+    "/home/nagolove/koh-mgc",
+    "/home/nagolove/koh-pomidor",
+    "/home/nagolove/koh-scripter",
+    "/home/nagolove/koh-stages",
+    "/home/nagolove/koh-t80",
+    "/home/nagolove/koh-t80-prev",
+    --"/home/nagolove/koh-we",
+
 }
