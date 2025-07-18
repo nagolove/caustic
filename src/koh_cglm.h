e8ab97b9d4eefb810dcd82f52fca4b91f27d7202
nagolove <dekar.own@gmail.com>
2025-07-18 13:52:20 +0300

    Файл с короткими именами функций cglm

---
+++ src/koh_cglm.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <cglm/cglm.h>
+#include <cglm/struct.h>
+
+//#define  vec2 glm_vec2
+#define  vec2_copy glm_vec2_copy
+#define  vec2_zero glm_vec2_zero
+#define  vec2_one glm_vec2_one
+#define  vec2_dot glm_vec2_dot
+#define  vec2_cross glm_vec2_cross
+#define  vec2_norm2 glm_vec2_norm2
+#define  vec2_norm glm_vec2_norm
+#define  vec2_add glm_vec2_add
+#define  vec2_adds glm_vec2_adds
+#define  vec2_sub glm_vec2_sub
+#define  vec2_subs glm_vec2_subs
+#define  vec2_mul glm_vec2_mul
+#define  vec2_scale glm_vec2_scale
+#define  vec2_scale_as glm_vec2_scale_as
+#define  vec2_div glm_vec2_div
+#define  vec2_divs glm_vec2_divs
+#define  vec2_addadd glm_vec2_addadd
+#define  vec2_subadd glm_vec2_subadd
+#define  vec2_muladd glm_vec2_muladd
+#define  vec2_muladds glm_vec2_muladds
+#define  vec2_maxadd glm_vec2_maxadd
+#define  vec2_minadd glm_vec2_minadd
+#define  vec2_subsub glm_vec2_subsub
+#define  vec2_addsub glm_vec2_addsub
+#define  vec2_mulsub glm_vec2_mulsub
+#define  vec2_mulsubs glm_vec2_mulsubs
+#define  vec2_maxsub glm_vec2_maxsub
+#define  vec2_minsub glm_vec2_minsub
+#define  vec2_negate_to glm_vec2_negate_to
+#define  vec2_negate glm_vec2_negate
+#define  vec2_normalize glm_vec2_normalize
+#define  vec2_normalize_to glm_vec2_normalize_to
+#define  vec2_rotate glm_vec2_rotate
+#define  vec2_center glm_vec2_center
+#define  vec2_distance2 glm_vec2_distance2
+#define  vec2_distance glm_vec2_distance
+#define  vec2_maxv glm_vec2_maxv
+#define  vec2_minv glm_vec2_minv
+#define  vec2_clamp glm_vec2_clamp
+#define  vec2_swizzle glm_vec2_swizzle
+#define  vec2_lerp glm_vec2_lerp
+#define  vec2_step glm_vec2_step
+#define  vec2_make glm_vec2_make
+#define  vec2_reflect glm_vec2_reflect
+#define  vec2_refract glm_vec2_refract
+
