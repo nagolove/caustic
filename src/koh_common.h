4e0ae8a57b52c6cd4c391b98ba72d7efac53d573
nagolove <dekar.own@gmail.com>
2025-08-27 18:39:29 +0300

    опечатка

--- src/koh_common.h
+++ src/koh_common.h
@@ -43,9 +43,8 @@ typedef int16_t     i16;
 typedef uint16_t    u16;
 typedef int8_t      i8;
 typedef uint8_t     u8;
-
 typedef float       f32;
-typedef double      f63;
+typedef double      f64;
 
 typedef struct { 
     f32 x, y; 
